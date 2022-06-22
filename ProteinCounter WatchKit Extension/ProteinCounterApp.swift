73fe61937e75ca41323a74a8c040e5bc4e126903
Lina Kang <dal07065@gmail.com>
2022-06-21 17:52:38 -0700

    Initial Commit

---
+++ ProteinCounter WatchKit Extension/ProteinCounterApp.swift
@@ -0,0 +1,19 @@
+//
+//  ProteinCounterApp.swift
+//  ProteinCounter WatchKit Extension
+//
+//  Created by Jonathan Miller on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ProteinCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
